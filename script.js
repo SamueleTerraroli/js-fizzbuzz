4ac071fe1b0df43839e490710eaf71ecbfd2551b
SamueleTerraroli <terraroli99@gmail.com>
2024-11-14 15:05:22 +0100

    feature: add for loop with conditional

---
+++ script.js
@@ -0,0 +1,19 @@
+
+for (let i=1; i<=100; i++) {
+    
+    if (!(i % 3)){
+        
+        showNumber= 'Fizz'    
+        console.log(showNumber);
+    } else if (!(i % 5)){
+    
+        showNumber= 'Buzz'    
+        console.log(showNumber);  
+    } else{
+        showNumber = i
+        console.log(showNumber);
+        
+    }
+    
+}
+
